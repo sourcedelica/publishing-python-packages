3c67faf6919a823ee9aadd86e0e1e31f16570b0e
Eric Pederson <ericacm+github@gmail.com>
2022-01-21 00:28:42 -0500

    Use squotes

--- test/test_harmonic_mean.py
+++ test/test_harmonic_mean.py
@@ -7,20 +7,20 @@ from imppkg.harmony import main
 
 
 @pytest.mark.parametrize(
-    ("inputs", "expected"), [
-        (["1", "4", "4"], 2.0),  # Happy path
-        (["a", "b"], 0.0),       # Bad input
+    ('inputs', 'expected'), [
+        (['1', '4', '4'], 2.0),  # Happy path
+        (['a', 'b'], 0.0),       # Bad input
         ([], 0.0),               # No input
     ]
 )
 def test_harmony_parameterized(monkeypatch, capsys, inputs, expected):
-    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+    monkeypatch.setattr(sys, 'argv', ['harmony'] + inputs)
 
     main()
 
     assert capsys.readouterr().out.strip() == colored(
         str(expected),
-        "red",
-        "on_cyan",
-        attrs=["bold"]
+        'red',
+        'on_cyan',
+        attrs=['bold']
     )
