451a494fb231a15f4280101bf319e33aca71a568
Eric Pederson <ericacm+github@gmail.com>
2022-01-21 00:29:13 -0500

    Format with black

--- test/test_harmonic_mean.py
+++ test/test_harmonic_mean.py
@@ -7,11 +7,12 @@ from imppkg.harmony import main
 
 
 @pytest.mark.parametrize(
-    ('inputs', 'expected'), [
+    ('inputs', 'expected'),
+    [
         (['1', '4', '4'], 2.0),  # Happy path
-        (['a', 'b'], 0.0),       # Bad input
-        ([], 0.0),               # No input
-    ]
+        (['a', 'b'], 0.0),  # Bad input
+        ([], 0.0),  # No input
+    ],
 )
 def test_harmony_parameterized(monkeypatch, capsys, inputs, expected):
     monkeypatch.setattr(sys, 'argv', ['harmony'] + inputs)
@@ -19,8 +20,5 @@ def test_harmony_parameterized(monkeypatch, capsys, inputs, expected):
     main()
 
     assert capsys.readouterr().out.strip() == colored(
-        str(expected),
-        'red',
-        'on_cyan',
-        attrs=['bold']
+        str(expected), 'red', 'on_cyan', attrs=['bold']
     )
